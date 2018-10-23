9ba104b8aaa926a6c11bd2188001d82fc9522101
demir6693 <demir_agovic@live.com>
2018-10-23 16:02:52 +0200

    login/reg

---
+++ MB_Web/Controllers/api/KorisnicisController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MB_Web;
+
+namespace MB_Web.Controllers.api
+{
+    public class KorisnicisController : ApiController
+    {
+        private Mobile_TownEntities db = new Mobile_TownEntities();
+
+        // GET: api/Korisnicis
+        public IQueryable<Korisnici> GetKorisnicis()
+        {
+            return db.Korisnicis;
+        }
+
+        // GET: api/Korisnicis/5
+        [ResponseType(typeof(Korisnici))]
+        public IHttpActionResult GetKorisnici(int id)
+        {
+            Korisnici korisnici = db.Korisnicis.Find(id);
+            if (korisnici == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(korisnici);
+        }
+
+        // PUT: api/Korisnicis/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutKorisnici(int id, Korisnici korisnici)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != korisnici.id_korisnika)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(korisnici).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!KorisniciExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Korisnicis
+        [ResponseType(typeof(Korisnici))]
+        public IHttpActionResult PostKorisnici(Korisnici korisnici)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Korisnicis.Add(korisnici);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = korisnici.id_korisnika }, korisnici);
+        }
+
+        // DELETE: api/Korisnicis/5
+        [ResponseType(typeof(Korisnici))]
+        public IHttpActionResult DeleteKorisnici(int id)
+        {
+            Korisnici korisnici = db.Korisnicis.Find(id);
+            if (korisnici == null)
+            {
+                return NotFound();
+            }
+
+            db.Korisnicis.Remove(korisnici);
+            db.SaveChanges();
+
+            return Ok(korisnici);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool KorisniciExists(int id)
+        {
+            return db.Korisnicis.Count(e => e.id_korisnika == id) > 0;
+        }
+    }
+}
